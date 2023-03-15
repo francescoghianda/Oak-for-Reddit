dcc2599eaa42ae9c18dcae3514050657844a01c2
Francesco Ghianda <francesco.ghianda@outlook.com>
2023-03-15 14:35:21 +0100

    Initial Commit

---
+++ Oak for Reddit/Oak_for_RedditApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oak_for_RedditApp.swift
+//  Oak for Reddit
+//
+//  Created by Francesco Ghianda on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Oak_for_RedditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
